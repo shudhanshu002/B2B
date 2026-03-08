44d75502287cf30c4ac6da101310676778df86e0
Sudhanshu singh <sudhanshu6073@gmail.com>
2026-03-08 22:32:51 +0530

    add to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,28 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { ProductForm } from './modules/CategoryGen/ProductForm';
+import { ProposalGen } from './modules/B2BProposal/ProposalGen';
+import { ImpactDashboard } from './modules/ImpactReport/ImpactDashboard';
+import { ChatBot } from './modules/ChatSupport/ChatBot';
+import Home from './pages/Home';
 
 function App() {
-
   return (
-    <h1>hi</h1>
-  )
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
+        <main className="max-w-5xl mx-auto p-8">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/analyze" element={<ProductForm />} />
+            <Route path="/proposals" element={<ProposalGen />} />
+            <Route path="/impact" element={<ImpactDashboard />} />
+            <Route path="/support" element={<ChatBot />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
